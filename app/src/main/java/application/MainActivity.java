9223c8f409c5238d8a9a9ea20c5b920e2e949c41
samueltexa <woolarsamuels@gmail.com>
2024-02-12 13:10:33 +0300

    init

---
+++ app/src/main/java/application/MainActivity.java
@@ -0,0 +1,16 @@
+package application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.orderwave.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
